5ec174eaf7956922182a4f4b0dfa3e6fdcfde9e5
Charlie0204 <chatogol_97@outlook.com>
2017-11-25 11:45:14 -0800

    agregado escribir y borrar

--- SQSf.py
+++ SQSf.py
@@ -3,22 +3,24 @@ import json
 
 def SqsRead():
     recibos = []
+    answerlist = []
     sqs = boto3.client('sqs')
     response = sqs.receive_message(
     QueueUrl='https://sqs.us-east-1.amazonaws.com/292274580527/cc406_team1',
     )
     for message in response["Messages"]:
-        recibos.append(json.loads(message["Body"]))
-    return recibos
+        recibos.append(message['ReceiptHandle'])
+        data = json.loads(message['Body'])
+        answerlist.append(data)
+        print(data)
+    for answer in answerlist:
+        mensaje = json.dumps(answer)
+        print(mensaje)
+        #respuesta = sqs.send_message(QueueUrl='https://sqs.us-east-1.amazonaws.com/292274580527/cc406_response1', MessageBody = mensaje)
+    #for r in recibos:
+        #response = sqs.delete_message(QueueUrl='https://sqs.us-east-1.amazonaws.com/292274580527/cc406_team1', ReceiptHandle = r)
 
-
-#def SqsDel():
-#    for r in recibos:
-#        response=sqs.delete_message(QueueURL='https://sqs.us-east-1.amazonaws.com/292274580527/cc406_team1',ReceiptHandle=r)
-
-rec = SqsRead()
-while(rec != None):
-    for i in range(10):
+SqsRead()
 
 #print(h[0]['orden']
 #for i in range(len(h)):

cb0e03634c90c380c7b7e7ec22e09679f2bde986
PepeGonza <31300194+PepeGonza@users.noreply.github.com>
2017-11-25 13:59:10 -0800

    Add files via upload

--- SQSf.py
+++ SQSf.py
@@ -3,24 +3,22 @@ import json
 
 def SqsRead():
     recibos = []
-    answerlist = []
     sqs = boto3.client('sqs')
     response = sqs.receive_message(
-    QueueUrl='https://sqs.us-east-1.amazonaws.com/292274580527/cc406_team1',
+        QueueUrl='https://sqs.us-east-1.amazonaws.com/292274580527/cc406_team1',
+        MaxNumberOfMessages = 10
     )
-    for message in response["Messages"]:
-        recibos.append(message['ReceiptHandle'])
-        data = json.loads(message['Body'])
-        answerlist.append(data)
-        print(data)
-    for answer in answerlist:
-        mensaje = json.dumps(answer)
-        print(mensaje)
-        #respuesta = sqs.send_message(QueueUrl='https://sqs.us-east-1.amazonaws.com/292274580527/cc406_response1', MessageBody = mensaje)
-    #for r in recibos:
-        #response = sqs.delete_message(QueueUrl='https://sqs.us-east-1.amazonaws.com/292274580527/cc406_team1', ReceiptHandle = r)
+    if response != None:
+        for message in response["Messages"]:
+            recibos.append(json.loads(message["Body"]))
+    return recibos
 
-SqsRead()
+
+#def SqsDel():
+#    for r in recibos:
+#        response=sqs.delete_message(QueueURL='https://sqs.us-east-1.amazonaws.com/292274580527/cc406_team1',ReceiptHandle=r)
+
+rec = SqsRead()
 
 #print(h[0]['orden']
 #for i in range(len(h)):
